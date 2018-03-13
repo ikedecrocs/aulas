88d8bc7d2670daaea7d5ad1b3a088798156bc73d
Aluno <mateuscaden@hotmail.com>
2018-03-13 09:34:48 -0300

    Maven

---
+++ projeto/helloworld/src/main/java/aula/App.java
@@ -0,0 +1,13 @@
+package aula;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "When the moon reach out the stars" );
+    }
+}
